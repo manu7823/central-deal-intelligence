0b813ba22eab63388779d16fc4585626697dbda2
Manuel Voppmann <manuel@voppmann.com>
2024-03-25 09:17:22 +0100

    fix: login layout

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -12,8 +12,8 @@ export default async function SignIn() {
   }
 
   return (
-    <>
-      <div className="flex justify-center height-screen-helper">
+    <div className="h-screen flex flex-col justify-between w-screen">
+      <div className="flex justify-center my-auto">
         <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
           <h1 className="text-4xl text-center mb-8">Login</h1>
           {/* <div className="flex justify-center pb-12 ">
@@ -23,6 +23,6 @@ export default async function SignIn() {
         </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
